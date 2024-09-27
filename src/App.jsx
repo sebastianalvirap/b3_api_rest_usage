f720639cef2284da3fc14f12218082c29daa0e8c
sebastianalvirap <juanoalvira97@gmail.com>
2024-09-26 20:09:30 -0500

    Carrusel

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ApiCoffeeCarruselBasic } from './assets/components/ApiCoffeeCarruselBasic'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <ApiCoffeeCarruselBasic />
+    </div>
+  )
+}
+
+export default App
